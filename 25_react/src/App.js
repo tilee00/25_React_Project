97ae0c578659c8aa068c4413b242a13e2082dd78
ti lee <tileeling1110@gmail.com>
2025-04-05 23:55:02 +0800

    random color project 2

--- 25_react/src/App.js
+++ 25_react/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian />
+      {/* <Accordian /> */}
+
+      {/* Random Color component */}
+      <RandomColor />
     </div>
   );
 }
