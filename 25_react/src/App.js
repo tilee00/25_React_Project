ecab2c67325bffb643fe40bd34aefb77816e54aa
ti lee <tileeling1110@gmail.com>
2025-04-13 22:16:13 +0800

    9-scroll indicator

--- 25_react/src/App.js
+++ 25_react/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load_more_data';
-import TreeView from './components/tree_view';
-import menus from './components/tree_view/data';
-import QRCodeGenerator from './components/qr_code_generator';
-import LightDarkMode from './components/light_dark_mode';
+import Accordian from './components/1-accordian';
+import RandomColor from './components/2-random-color';
+import StarRating from './components/3-star-rating';
+import ImageSlider from './components/4-image-slider';
+import LoadMoreData from './components/5-load_more_data';
+import TreeView from './components/6-tree_view';
+import menus from './components/6-tree_view/data';
+import QRCodeGenerator from './components/7-qr_code_generator';
+import LightDarkMode from './components/8-light_dark_mode';
+import ScrollIndicator from './components/9-scroll_indicator';
 
 function App() {
   return (
@@ -34,7 +35,10 @@ function App() {
       {/* <QRCodeGenerator /> */}
 
       {/* Light and dark theme switch */}
-      <LightDarkMode />
+      {/* <LightDarkMode /> */}
+
+      {/* Scroll indicator component */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
   );
 }
--- 25_react/src/components/9-scroll_indicator/index.jsx
+++ 25_react/src/components/9-scroll_indicator/index.jsx
@@ -1,13 +1,79 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import "./scroll.css"
 
-export default function ScrollIndicator({url}) {
+export default function ScrollIndicator({ url }) {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [scrollPercentage, setScrollPercentage] = useState(0);
 
-    
+    async function fetchData(getUrl) {
+        try {
+            setLoading(true);
+            // Sends a GET request to the URL stored in getUrl
+            const response = await fetch(getUrl);
+            // Parses the JSON response into a JavaScript object
+            const data = await response.json();
 
-    return <div>
+            if (data && data.products && data.products.length > 0) {
+                setData(data.products);
+                setLoading(false);
+            }
+        } catch (e) {
+            console.log(e);
+            setErrorMessage(e.message);
+        }
+    }
 
-    </div>
+    useEffect(() => {
+        fetchData(url);
+    }, [url]);
+
+    function handleScrollPercentage() {
+        console.log(document.body.scrollTop, document.documentElement.scrollTop, document.documentElement.scrollHeight, document.documentElement.clientHeight);
+
+        const howMuchScorlled = document.body.scrollTop || document.documentElement.scrollTop;
+
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+        setScrollPercentage(Math.round((howMuchScorlled / height) * 100));
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScrollPercentage)
+
+        return () => {
+            window.removeEventListener("scroll", () => {
+
+            })
+        }
+    }, []);
+
+    console.log(data, scrollPercentage);
+
+    if (errorMessage) {
+        return <div>Error! {errorMessage}</div>
+    }
+
+    if (loading) {
+        return <div>Loading data! Please wait</div>
+    }
+
+    return (
+        <div className="container">
+            <div className="top_container">
+                <h1>Custom Scroll Indicator ({scrollPercentage}%) </h1>
+                <div className="scroll_progress_tracking_container">
+                    <div className="current_progress_bar" style={{ width: `${scrollPercentage}%` }}></div>
+                </div>
+            </div>
+            <div className="data_container">
+                {
+                    data && data.length > 0 ?
+                        data.map((dataItem) => <p>{dataItem.title}</p>)
+                        : null
+                }
+            </div>
+        </div>)
 }
