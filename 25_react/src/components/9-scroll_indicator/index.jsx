2ad469f4c5c7b99eb62ae01ad11a8baa53ab481b
ti lee <tileeling1110@gmail.com>
2025-04-13 20:00:16 +0800

    update file name

---
+++ 25_react/src/components/9-scroll_indicator/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function ScrollIndicator({url}) {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    
+
+    return <div>
+
+    </div>
+}
